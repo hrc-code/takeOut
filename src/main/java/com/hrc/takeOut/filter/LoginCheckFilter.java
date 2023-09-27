936f974b1a119c009111c7666fd0e0ad0058337f
2958934551 <2958934551@qq.com>
2023-09-27 15:56:32 +0800

    style: 删除无用导包

--- src/main/java/com/hrc/takeOut/filter/LoginCheckFilter.java
+++ src/main/java/com/hrc/takeOut/filter/LoginCheckFilter.java
@@ -1,6 +1,6 @@
 package com.hrc.takeOut.filter;
 
-import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson.JSON;
 import com.hrc.takeOut.commom.Result;
 import com.hrc.takeOut.utils.ThreadLocals;
 import lombok.extern.slf4j.Slf4j;
